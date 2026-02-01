214ebb63abc005019c5f7e1bf696e233339c44db
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 06:35:04 +0800

    feat: update Mockapi
    add updateRoom(),
    make sure the updates are reflected

--- src/app/mockapi.ts
+++ src/app/mockapi.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, filter, map } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, filter, map, tap } from 'rxjs';
 import { Room } from './room';
 import { environment } from '../environments/environment';
 
@@ -8,10 +8,34 @@ import { environment } from '../environments/environment';
   providedIn: 'root',
 })
 export class Mockapi {
-  private http = inject(HttpClient);
   private baseURL = `https://${environment.mockApiKey}.mockapi.io/rooms`;
+  private dataSubject = new BehaviorSubject<Room[]>([]);
+  data$ = this.dataSubject.asObservable();
 
-  getAllRoom(): Observable<Room[]> {
-    return this.http.get<Room[]>(this.baseURL);
+  constructor(private http: HttpClient){
+    // load on service initialisation
+    this.getAllRoom();
+  }
+
+  getAllRoom(): void {
+    this.http.get<Room[]>(this.baseURL).subscribe(data =>{
+      this.dataSubject.next(data);
+    })
+  }
+
+  updateRoom(id: number, updatedData:Room): Observable<Room> {
+    const url = `${this.baseURL}/${id}`;
+    return this.http.put<Room>(url, updatedData).pipe(
+      tap(
+        updatedRoom => {
+          const currentData = this.dataSubject.value;
+          const index = currentData.findIndex(item => item.id === id);
+          if(index != -1){
+            currentData[index] = updatedRoom;
+            this.dataSubject.next([...currentData]);
+          }
+        }
+      )
+    )
   }
 }
