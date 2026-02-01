a033c6cdd30bdb571d3de1409bd0b16ad34725c4
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 06:26:54 +0800

    feat: update UserBookings component

--- src/app/user-bookings/user-bookings.ts
+++ src/app/user-bookings/user-bookings.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorage } from '../local-storage';
 
 @Component({
   selector: 'user-bookings',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './user-bookings.css',
 })
 export class UserBookings {
-
+  constructor(private localStore: LocalStorage) {
+    this.bookings = localStore.getData(this.localKey);
+  }
+  localKey = 'bookings';
+  bookings;
 }
