610730603e20395784765a2dcf1b378ef26f25e4
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 06:29:19 +0800

    feat: update Home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { RoomCardComponent } from '../room-card-component/room-card-component';
 import { CommonModule } from '@angular/common';
 import { Mockapi } from '../mockapi';
-import { map, Observable, Subscription} from 'rxjs';
+import { map, Observable, Subscription } from 'rxjs';
 import { Room } from '../room';
 import { NgbAlert } from '@ng-bootstrap/ng-bootstrap/alert';
 import { AlertService } from '../alert-service';
@@ -14,17 +14,15 @@ import { AlertService } from '../alert-service';
   styleUrl: './home.css',
 })
 
-export class Home implements OnInit {
+export class Home implements OnInit{
   roomService: Mockapi = inject(Mockapi);
-  constructor(private alertService: AlertService){}
-  data$!: Observable<Room[]>;
+  constructor(private alertService: AlertService) {}
+  data$: Observable<Room[]> = this.roomService.data$;
   filteredRoom$!: Observable<Room[]>;
 
-  alertMessage$!:Observable<string>;
-  // alertMessage:string = this.alertMessage$.toString().substring(0,5);
-// alertMessage = '';
+  alertMessage$!: Observable<string>;
+
   ngOnInit(): void {
-    this.data$ = this.roomService.getAllRoom();
     this.filteredRoom$ = this.data$;
     this.alertMessage$= this.alertService.getAlert();
   }
