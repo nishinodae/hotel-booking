65905c72220ee556c31eccbb071be10bb536ae26
nishinodae <masyitahsalwa@gmail.com>
2026-02-01 22:36:59 +0800

    feat: update room-card-component

--- src/app/room-card-component/room-card-component.ts
+++ src/app/room-card-component/room-card-component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { Room } from '../room';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-room-card-component',
-  imports: [],
+  selector: 'room-card-component',
+  imports: [CommonModule],
   templateUrl: './room-card-component.html',
   styleUrl: './room-card-component.css',
 })
 export class RoomCardComponent {
-
+  @Input() room!: Room;
 }
