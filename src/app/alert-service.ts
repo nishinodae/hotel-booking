f6a4e432da067d9afbf3d7d9ad52e50b87f76b22
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 02:10:29 +0800

    feat: add alert-service

---
+++ src/app/alert-service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertService {
+  private alertMessage = new Subject<string>;
+
+  sendAlert(message: string) {
+    this.alertMessage.next(message);
+    console.log(message);
+  }
+
+  getAlert(): Observable<string> {
+    return this.alertMessage;
+  }
+}
