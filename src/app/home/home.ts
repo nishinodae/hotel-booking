37a7a2207c95b80636a3da4234070aabd788b980
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 02:14:34 +0800

    feat: update home component to use AlertService and display alert message

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,25 +1,32 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { RoomCardComponent } from '../room-card-component/room-card-component';
 import { CommonModule } from '@angular/common';
 import { Mockapi } from '../mockapi';
-import { map, Observable } from 'rxjs';
+import { map, Observable, Subscription} from 'rxjs';
 import { Room } from '../room';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap/alert';
+import { AlertService } from '../alert-service';
 
 @Component({
   selector: 'app-home',
-  imports: [RoomCardComponent, CommonModule],
+  imports: [RoomCardComponent, CommonModule, NgbAlert],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 
 export class Home implements OnInit {
   roomService: Mockapi = inject(Mockapi);
+  constructor(private alertService: AlertService){}
   data$!: Observable<Room[]>;
   filteredRoom$!: Observable<Room[]>;
 
+  alertMessage$!:Observable<string>;
+  // alertMessage:string = this.alertMessage$.toString().substring(0,5);
+// alertMessage = '';
   ngOnInit(): void {
     this.data$ = this.roomService.getAllRoom();
     this.filteredRoom$ = this.data$;
+    this.alertMessage$= this.alertService.getAlert();
   }
 
   getFilteredRoom(roomType: string) {
@@ -31,4 +38,5 @@ export class Home implements OnInit {
   getAllRoom() {
     this.filteredRoom$ = this.data$;
   }
+
 }
