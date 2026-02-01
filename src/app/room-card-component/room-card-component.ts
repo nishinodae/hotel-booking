1027e1550ba839b08010650d087e68956216767a
nishinodae <masyitahsalwa@gmail.com>
2026-02-01 22:44:04 +0800

    feat: update 'Book now' button to open BookingForm modal

--- src/app/room-card-component/room-card-component.ts
+++ src/app/room-card-component/room-card-component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, Input } from '@angular/core';
 import { Room } from '../room';
 import { CommonModule } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap/modal';
+import { BookingForm } from '../booking-form/booking-form';
 
 @Component({
   selector: 'room-card-component',
@@ -10,4 +12,10 @@ import { CommonModule } from '@angular/common';
 })
 export class RoomCardComponent {
   @Input() room!: Room;
+  private modalService = inject(NgbModal);
+
+  open() {
+    const modalRef = this.modalService.open(BookingForm, { size: 'xl', centered: true },);
+    modalRef.componentInstance.room = this.room;
+  }
 }
