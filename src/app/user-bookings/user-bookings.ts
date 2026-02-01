0b6acd9ff821d32d78c0608067757629108176ac
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 02:15:04 +0800

    feat: add user-bookings component

---
+++ src/app/user-bookings/user-bookings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-bookings',
+  imports: [],
+  templateUrl: './user-bookings.html',
+  styleUrl: './user-bookings.css',
+})
+export class UserBookings {
+
+}
