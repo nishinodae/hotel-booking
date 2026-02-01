ca9e2c9e5f545b7a1a9fdcbb74cf5116bec4022b
nishinodae <masyitahsalwa@gmail.com>
2026-02-02 02:15:33 +0800

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { UserBookings } from './user-bookings/user-bookings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'HotelBooking.com'
+    },
+    {
+        path:'bookings',
+        component: UserBookings,
+        title: 'HotelBooking.com | My Bookings'
+    }
+];
